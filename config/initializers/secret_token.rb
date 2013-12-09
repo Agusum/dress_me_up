7025026e93ad3c5b18560c8e3affa7d4419f7b1f
Agusum <agusdelavilla@gmail.com>
2013-12-09 12:24:19 +0000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DressMeUp::Application.config.secret_key_base = '13ebedb8d2a202049014d61fb1eabacd96963e37c0bdad7527cfc829da92145e479e1c97182e85cbb8cf28c18b4b90b58f5dfab450f7e0beeef2952619be247d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DressMeUp::Application.config.session_store :cookie_store, key: '_dress_me_up_session'
